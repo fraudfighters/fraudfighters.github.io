0230350086756de48a534c6626334b903d2df54c
Paul Conyngham <paul.steven.conyngham@gmail.com>
2020-05-14 15:19:04 +1000

    first commit

--- old/assets/js/pathfinders.js
+++
@@ -1,8 +0,0 @@
-(function( $ ){
-    var $navbar = $('.navbar');
-    var $logo = $('.brand img');
-    $(window).scroll(function(){
-        $navbar.toggleClass('scrolled', $(this).scrollTop() > 610 )
-    });
-    
-}( $ ));
